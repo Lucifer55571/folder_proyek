70cfd042eea761c140a2fed916788c5824304c55
Lucifer55571 <lucifer55571@gmail.com>
2026-01-23 07:25:08 +0700

    perbaikan route

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 # --- DATA KRIPTOGRAFI (Dari Kode Anda) ---
 alphabet = "abcdefghijklmnopqrstuvwxyz1234567890-,.!@#$%^&*()=+?"
